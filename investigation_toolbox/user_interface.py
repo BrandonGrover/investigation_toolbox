90c9e961fed43924b01d1b3c5ad4a24825263b94
zPhr34k <zef43ak@gmail.com>
2022-06-01 20:17:24 -0400

    restructure

--- investigation_toolbox/user_interface.py
+++ investigation_toolbox/user_interface.py
@@ -1,7 +1,11 @@
-import modules
+from investigation_toolbox.src.email.email_queries import email_queries
+from investigation_toolbox.src.hostname.hostname_queries import host_queries
+from investigation_toolbox.src.ipv4.ipv4_queries import ip_queries
+import logger
 import os
-import config
-result = "#" * 120 + "\n" + "#" * 48 + " " * 8 + " Result " + " " * 8 + "#" * 48 + "\n" + "#" * 120
+import docs.conf as conf
+
+
 #Menu
 os.system('cls')
 run = True
@@ -25,13 +29,13 @@ while(run):
         email_selector = input("Choose an option: ")
         user_email_input = input("What is the email address?: ")
         if email_selector == "1":
-            print(result)
-            email_rep_result = modules.email_queries(user_email_input).email_rep()
+            print(conf.result)
+            email_rep_result = email_queries(user_email_input).email_rep()
             print(email_rep_result)
             continue
         if email_selector == "2":
-            print(result)
-            email_breach_result = modules.email_queries(user_email_input).breach_directory()
+            print(conf.result)
+            email_breach_result = email_queries(user_email_input).breach_directory()
             print(email_breach_result)
             continue
         else:
@@ -45,8 +49,8 @@ while(run):
         ip_selector = input("Choose an option: ")
         user_ipv4_input = input("What is the IPv4 address?: ")
         if ip_selector == "1":
-            print(result)
-            ipdb_ipv4_result = modules.ip_queries(user_ipv4_input).abuse_ipdb()
+            print(conf.result)
+            ipdb_ipv4_result = ip_queries(user_ipv4_input).abuse_ipdb()
             print(ipdb_ipv4_result)
             continue
         else:
@@ -60,8 +64,8 @@ while(run):
         host_selector = input("Choose an option: ")
         user_host_input = input("What is the domain name?: ")
         if host_selector == "1":
-            print(result)
-            whois_result = modules.host_queries(user_host_input).whois_lookup()
+            print(conf.result)
+            whois_result = host_queries(user_host_input).whois_lookup()
             print(whois_result)
             continue
         else:
